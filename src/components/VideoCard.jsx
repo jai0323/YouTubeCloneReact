2fcc6c16c1d7b63d75cc3ac5f9852bb40805d25e
jai0323 <jaishobhit45@gmail.com>
2024-03-05 23:31:00 +0530

    List Item Description

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,10 +1,15 @@
 import  {Card, Container, Row, Col} from 'react-bootstrap'
 const VideoCard = ({item}) =>{
-  console.log(item)
+  //console.log(item)
+  let title = item.snippet.title;
+  if(title.length>40)
+  {
+    title= title.slice(0,50)+"..."; 
+  }
 return <Container style={{marginBottom:50, height:80}}>
   <Row>
-    <Col sm='6'><Card.Img variant="top" src={item.snippet.thumbnails.default.url} /></Col>
-    <Col sm='6' className="text-wrap"  style={{height:50}}>{item.snippet.title}</Col>
+    <Col sm='6'><Card.Img variant="top" src={item.snippet.thumbnails.default.url}/></Col>
+    <Col sm='6' className="text-wrap"  style={{height:50}}>{title}</Col>
   </Row>
 </Container>
 }
